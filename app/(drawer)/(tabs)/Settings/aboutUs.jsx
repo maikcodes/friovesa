226b930d96cb5a9afaf6710f166bbb8d3f8b2cdb
Willy Maykros Romero <94816128+maikcodes@users.noreply.github.com>
2024-07-18 03:32:44 -0500

    Refactor: add padding-bottom to aboutUs and termsAndConditions pages

--- app/(drawer)/(tabs)/Settings/aboutUs.jsx
+++ app/(drawer)/(tabs)/Settings/aboutUs.jsx
@@ -5,7 +5,7 @@ export default function AboutUs() {
   return (
     <CustomSafeAreaView>
       <CustomWebView
-        htmlContent={`<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>${aboutUsInfo}</body><style> h1, h2, h3, h4 { color: #003e80; } img { width: 100%; } </style></html>`}
+        htmlContent={`<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>${aboutUsInfo}</body><style> h1, h2, h3, h4 { color: #003e80; } img { width: 100%; } body { padding-bottom: 30px; } </style></html>`}
       />
     </CustomSafeAreaView>
   );
--- app/(drawer)/(tabs)/Settings/termsAndConditions.jsx
+++ app/(drawer)/(tabs)/Settings/termsAndConditions.jsx
@@ -5,8 +5,8 @@ export default function TermsAndConditions() {
   return (
     <CustomSafeAreaView>
       <CustomWebView
-        htmlContent={`<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>${termsAndConditionsInfo}</body><style> h1, h2, h3, h4 { color: #003e80; } </style></html>`}
-      ></CustomWebView>
+        htmlContent={`<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>${termsAndConditionsInfo}</body><style> h1, h2, h3, h4 { color: #003e80; } body { padding-bottom: 30px; } </style></html>`}
+      />
     </CustomSafeAreaView>
   );
 }
