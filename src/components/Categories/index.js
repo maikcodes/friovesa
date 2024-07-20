0c2c5399aed6d19496c7a931c3b661bf16d3b800
Willy Maykros Romero <94816128+maikcodes@users.noreply.github.com>
2024-07-20 17:28:34 -0500

    refactor: Remove CategoriesList and ProductsList components

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,4 +1,3 @@
-export { default as CategoriesList } from "./CategoriesList";
 export { default as CategoriesScrollableMenu } from "./CategoriesScrollableMenu";
 export { default as CategoryCard } from "./CategoryCard";
 export * from "./skeletons";
